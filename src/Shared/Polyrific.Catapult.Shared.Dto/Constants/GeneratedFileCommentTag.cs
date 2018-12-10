8d45824f9e04bcd15338ebfdca51db79179a82ac
BrainBalaka <xainsz.balaka@gmail.com>
2018-12-10 09:06:05 +0700

    Add helper functions in CodeGeneratorProvider

---
+++ src/Shared/Polyrific.Catapult.Shared.Dto/Constants/GeneratedFileCommentTag.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Polyrific, Inc 2018. All rights reserved.
+
+namespace Polyrific.Catapult.Shared.Dto.Constants
+{
+    public static class GeneratedFileCommentTag
+    {
+        public const string CsharpBeginComment = "// Begin auto-generated content: {0}";
+        public const string CsharpEndComment = "// End auto-generated content: {0}";
+        public const string CshtmlBeginComment = "<!--Begin auto-generated conent: {0}-->";
+        public const string CshtmlEndComment = "<!--End auto-generated content: {0}-->";
+        public const string JavascriptBeginComment = "// Begin auto-generated content: {0}";
+        public const string JavascriptEndComment = "/*End auto-generated content: {0}*/";
+        public const string CsharpModelTag = "// OpenCatapultModelId:{0}";
+        public const string CshtmlModelTag = "<!--OpenCatapultModelId:{0}-->";
+        public const string JavascriptModelTag = "// OpenCatapultModelId:{0}-->";
+    }
+}
---
+++ src/Shared/Polyrific.Catapult.Shared.Dto/Constants/GeneratedFileType.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Polyrific, Inc 2018. All rights reserved.
+
+namespace Polyrific.Catapult.Shared.Dto.Constants
+{
+    public static class GeneratedFileType
+    {
+        public const string CSharp = "cs";
+        public const string CsHtml = "cshtml";
+        public const string Javascript = "js";
+    }
+}
